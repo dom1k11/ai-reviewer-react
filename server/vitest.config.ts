8b0d40e2f15ab8ef67eab37d3b9e0f0d9c43ba19
Dominik Sedusov <dom1k11@outlook.com>
2025-11-29 18:57:37 +0200

    updated vitest config

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -5,5 +5,33 @@ export default defineConfig({
     globals: true,
     environment: "node",
     setupFiles: ["./test/setup.ts"],
+
+    coverage: {
+      provider: "v8",
+      reportsDirectory: "./coverage",
+
+      exclude: [
+        "**/node_modules/**",
+        "**/dist/**",
+        "**/build/**",
+        "**/*.config.*",
+        "src/**/*.{test,spec}.{ts,js}",
+        "test/**",
+        "src/index.ts",
+        "src/app.ts",
+        "src/cors_config.ts",
+        "src/db/**",
+        "src/db/migrations/**",
+        "src/db/seeds/**",
+        "src/clients/**",
+        "src/config/**",
+        "src/validators/**",
+        "src/middleware/validate.ts",
+        "src/types/**",
+        "src/utils/",
+        "src/queries/",
+        "src/routes/"
+      ]
+    },
   },
 });
