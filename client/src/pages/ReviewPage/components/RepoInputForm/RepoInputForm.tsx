1bb6b85a0a6baa7c4a456c1fc670484f84b6860b
Dominik Sedusov <dom1k11@outlook.com>
2025-12-02 12:42:48 +0200

    Selecting criterias and applying to request

--- client/src/pages/ReviewPage/components/RepoInputForm/RepoInputForm.tsx
+++ client/src/pages/ReviewPage/components/RepoInputForm/RepoInputForm.tsx
@@ -7,10 +7,10 @@ export default function RepoInputForm({ onReviewReady }) {
   const [repoUrl, setRepoUrl] = useState(
     "https://github.com/dom1k11/code-template"
   );
-
+  const [criteria, setCriteria] = useState([]);
+  console.log(criteria);
   async function handleSend() {
-    console.log("Send request:", repoUrl);
-    const review = await getReview(repoUrl); //TODO Send requirements from ReqSelector
+    const review = await getReview(repoUrl, criteria);
     onReviewReady(review);
   }
 
@@ -27,7 +27,8 @@ export default function RepoInputForm({ onReviewReady }) {
         onChange={(e) => setRepoUrl(e.target.value)}
       />
 
-      <ReqSelector></ReqSelector>
+      <ReqSelector criteria={criteria} onChangeCriteria={setCriteria} />
+
       <button className="btn btn-primary send-btn" onClick={handleSend}>
         Get review
       </button>
