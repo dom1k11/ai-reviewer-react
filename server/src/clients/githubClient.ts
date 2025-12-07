b8c704841f486414603896371657714a9071e7a2
Dominik Sedusov <dom1k11@outlook.com>
2025-12-07 15:56:04 +0200

    deploy fix

--- server/src/clients/githubClient.ts
+++ server/src/clients/githubClient.ts
@@ -1,14 +1,20 @@
-import { Octokit } from "@octokit/core";
-
-const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-
-export async function githubRequest<T = unknown>(endpoint: string, params: object): Promise<T> {
+export async function githubRequest<T = unknown>(
+  endpoint: string,
+  params: object
+): Promise<T> {
   try {
+    const { Octokit } = await import("@octokit/core");
+
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    });
+
     const res = await octokit.request(endpoint, {
       ...params,
       headers: { "X-GitHub-Api-Version": "2022-11-28" },
     });
-    return res.data;
+
+    return res.data as T;
   } catch (err) {
     console.error("❌ GitHub request failed:", err);
     throw new Error("GitHub API request failed");
