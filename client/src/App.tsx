072dc8558241a5232646717eb26800562aa86628
Dominik Sedusov <dom1k11@outlook.com>
2025-12-08 15:07:35 +0200

    redirecting improvements

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,40 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ReviewPage from "./pages/ReviewPage/ReviewPage";
-import LandingPage from "./pages/LandingPage/LandingPage";
 import StatsPage from "./pages/StatsPage/StatsPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import { isLoggedIn } from "./helpers/auth";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/app" element={<ReviewPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/stats" element={<StatsPage />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route
+          path="/login"
+          element={
+            isLoggedIn() ? <Navigate to="/app" replace /> : <LoginPage />
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            isLoggedIn() ? <Navigate to="/app" replace /> : <RegisterPage />
+          }
+        />
+        <Route
+          path="/app"
+          element={
+            isLoggedIn() ? <ReviewPage /> : <Navigate to="/login" replace />
+          }
+        />
+        <Route
+          path="/stats"
+          element={
+            isLoggedIn() ? <StatsPage /> : <Navigate to="/login" replace />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
